b398fae9ecabe7ab85a7b70a390cf2f467e96f52
Marcello Herreshoff <marcello.herreshoff@gmail.com>
2020-04-03 13:42:03 -0700

    make_json

---
+++ make_json.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+import json
+
+etym_f = open('etymwn-20130208/etymwn.tsv', 'r')
+
+etymologies = defaultdict(list)
+for line in etym_f:
+    word1, rel, word2 = line.split("\t")
+    if rel != "rel:etymology": continue
+    etymologies[word1].append(word2)
+
+def get_languages(w, s):
+    lang, word = w.split(": ")
+    s.add(lang)
+    for w2 in etymologies[w]:
+        get_languages(w2, s)
+
+every_language = set()
+result = {}
+for lang_word in sorted(etymologies.keys()):
+    lang, word = lang_word.split(": ")
+    every_language.add(lang)
+    if lang == "eng":
+        etym_languages = set()
+        get_languages(lang_word, etym_languages)
+        result[word] = sorted(etym_languages)
+
+result['_EVERY_LANGUAGE'] = sorted(every_language)
+
+
+with open('languages.json', 'w') as f:
+    json.dump(result, f)
